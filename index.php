f7fb32b7363bb79bb4dc43a7f0df8039817b3a69
Finney243 <finneysiva4@gmail.com>
2025-06-16 21:33:37 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Muhindo Sivayitira Finney PHP</title>
+</head>
+<body>
+  <h1>Muhindo Sivayitira Finney PHP</h1>
+
+  <pre>
+    M   M
+    MM MM
+    M M M
+    M   M
+    M   M
+  </pre>
+
+  <p>
+    SHA256 of "Muhindo Sivayitira Finney" is:
+    <?php
+      echo hash('sha256', 'Muhindo Sivayitira Finney');
+    ?>
+  </p>
+</body>
+</html>
